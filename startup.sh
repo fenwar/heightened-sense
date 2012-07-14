e09de1887a5a493c76c347cbfadd1584f0acf8c9
Paul Fenney <fenney@gmail.com>
2012-07-14 23:59:56 +0100

    Added startup script to kill the existing DvdPlayer application and run one connected to the FIFO.

--- startup.sh
+++ startup.sh
@@ -7,4 +7,14 @@
 rm -rf /tmp/www
 ln -s /tmp/ramfs/server/heightened-sense/www /tmp/www
 
+# Kill the running DvdPlayer app and any root apps
+/usr/bin/stopall
+
+# Create our FIFO for sending web keystrokes to DvdPlayer 
+rm /tmp/webkeys
+mknod /tmp/webkeys p
+
+# Start DvdPlayer listening for keystrokes
+cd /usr/local/bin
+tail -f -s0 /tmp/webkeys | DvdPlayer &
 
